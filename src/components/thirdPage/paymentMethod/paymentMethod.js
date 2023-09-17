f320ade5005011fdb3e86082c19e5d386f7d2085
surekhaBhas <tsurekha3@gmail.com>
2023-09-17 12:37:55 +0530

    updated

--- src/components/thirdPage/paymentMethod/paymentMethod.js
+++ src/components/thirdPage/paymentMethod/paymentMethod.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import  { useState } from 'react'
 import './paymentMethod.css';
 import {AiOutlineClose,AiFillWallet} from 'react-icons/ai'
 import {BiSolidCreditCardAlt} from 'react-icons/bi'
@@ -6,34 +6,34 @@ import {BsCreditCard2FrontFill} from 'react-icons/bs';
 import {HiCreditCard} from 'react-icons/hi';
 import {RiBankCardFill} from 'react-icons/ri'
 
- class PaymentMethod extends Component {
-  constructor(){
-    super()
-    this.state = {
-      selectedMethod:"Credit/Debit Cards",
-    };
-  }
-  handleButtonClick = (method) => {
-    this.setState({ selectedMethod: method });
+import { Link,useLocation } from 'react-router-dom';
+
+function PaymentMethod() {
+  const location=useLocation();
+  
+  const [selectedMethod,setSelectMethod]=useState("Credit/Debit Cards")
+  const searchParams = new URLSearchParams(location.search);
+  let cost=searchParams.get('cost')
+  const paymentMethods = [
+    { icon: <RiBankCardFill/>, label: 'Credit/Debit Cards' },
+    { icon: <AiFillWallet/>, label: 'Wallets' },
+    { icon: <BiSolidCreditCardAlt/>, label: 'UPI' },
+    { icon: <BsCreditCard2FrontFill/>, label: 'Credit' },
+    { icon: <RiBankCardFill/>, label: 'Net Banking' },
+    { icon: <HiCreditCard />, label: 'Food Cards' }
+  ];
+  const  handleButtonClick = (method) => {
+    setSelectMethod(method);
   };
-  render() {
-    const paymentMethods = [
-      { icon: <RiBankCardFill/>, label: 'Credit/Debit Cards' },
-      { icon: <AiFillWallet/>, label: 'Wallets' },
-      { icon: <BiSolidCreditCardAlt/>, label: 'UPI' },
-      { icon: <BsCreditCard2FrontFill/>, label: 'Credit' },
-      { icon: <RiBankCardFill/>, label: 'Net Banking' },
-      { icon: <HiCreditCard />, label: 'Food Cards' }
-    ];
-    return (
-      <div className='bg-container'>
+  return (
+    <div className='bg-container'>
       <div className='payment-container'>
         <button className='close'><AiOutlineClose/></button>
         <h2 className='title'>Choose payment method</h2>
         <div className='payment-card'>
             <ul className='method-container'>
-                {paymentMethods.map((method,index)=><li key={index} className={ this.state.selectedMethod === method.label ? 'selected' : ''}><button  className='button'
-                  onClick={() => this.handleButtonClick(method.label)}>{method.icon}{method.label}</button></li>)}
+                {paymentMethods.map((method,index)=><li key={index} className={ selectedMethod === method.label ? 'selected' : ''}><button  className='button'
+                  onClick={() => handleButtonClick(method.label)}>{method.icon}{method.label}</button></li>)}
             </ul>
             <div className='card-details'>
               <p className='add'>Add new card</p>
@@ -55,15 +55,17 @@ import {RiBankCardFill} from 'react-icons/ri'
                 <label  htmlFor='conditions'>
                   Securely save this card for a faster checkout next time.
                   </label></div>
-                  <button className='pay'>PAY `133</button>
+                <Link to='/'>  <button className='pay'>PAY {cost}</button></Link>
               </form>
               <p className='description'>card details will be saved securely. based of the industry standard</p>
             </div>
         </div> 
       </div>
       </div>
-    )
-  }
+  )
 }
 
 export default PaymentMethod
+
+ 
+ 
